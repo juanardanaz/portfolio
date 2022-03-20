dcb1cdccbe5e5dbc2d6f3a3f701271a672b9aaaf
Juan <juancruzardanaz@gmail.com>
2022-03-20 19:06:28 -0300

    nav shadow adjustment

--- script/app.js
+++ script/app.js
@@ -2,7 +2,11 @@
 function scrollHeader(){
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 50) {
+        header.classList.add('scroll_header');
+    } else {
+        header.classList.remove('scroll_header');
+    }
 }
 window.addEventListener('scroll', scrollHeader);
 
