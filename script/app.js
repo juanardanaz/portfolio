9eb098254dbe17f890d61ffd9d324c04ab8a86c5
Juan <juancruzardanaz@gmail.com>
2022-06-05 21:26:11 -0300

    full design upgrade

--- script/app.js
+++ script/app.js
@@ -10,11 +10,10 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader);
 
-
-/*=============== SERVICES MODAL ===============*/
-const modalViews = document.querySelectorAll('.services_modal');
-const modalBtns = document.querySelectorAll('.services_button'); 
-const modalClose = document.querySelectorAll('.services_modal_close');
+/*=============== RESUME MODAL ===============*/
+const modalViews = document.querySelectorAll('.resume_modal');
+const modalBtns = document.querySelectorAll('.resume_button'); 
+const modalClose = document.querySelectorAll('.resume_modal_close');
 
 let modal = function(modalClick) {
     modalViews[modalClick].classList.add('active_modal');
@@ -35,9 +34,9 @@ modalClose.forEach((mc) => {
 });
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-let mixerPortfolio = mixitup('.work_container', {
+let mixerPortfolio = mixitup('.projects_content', {
     selectors: {
-        target: '.work_card'
+        target: '.projects_card'
     },
     animation: {
         duration: 300
@@ -77,56 +76,30 @@ let swiperTestimonial = new Swiper(".testimonial_container", {
     },
 });
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+/*=============== DARK LIGHT THEME ===============*/
+const btnSwitch = document.getElementById('theme-button');
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    
+    //LocalStorage
+    if (document.body.classList.contains('dark-theme')) {
+        localStorage.setItem('dark', 'true');
+        btnSwitch.setAttribute('src', './assets/img/bx-sun.svg');
+    } else {
+        localStorage.setItem('dark', 'false');
+        btnSwitch.setAttribute('src', './assets/img/bx-moon.svg');
+    }
+});
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 58,
-            sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+// We obtain the actual theme
+if (localStorage.getItem('dark') === 'true') {
+    document.body.classList.add('dark-theme');
+    btnSwitch.setAttribute('src', './assets/img/bx-sun.svg');
+} else {
+    document.body.classList.remove('dark-theme');
+    btnSwitch.setAttribute('src', './assets/img/bx-moon.svg');
 }
-window.addEventListener('scroll', scrollActive)
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-const themeButton = document.getElementById('theme-button')
-const lightTheme = 'light_theme'
-const iconTheme = 'bx-sun'
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the light-theme class
-const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
-    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the light / icon theme
-    document.body.classList.toggle(lightTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
@@ -137,6 +110,8 @@ const sr = ScrollReveal({
     // reset: true,
 });
 
-sr.reveal(`.home_data`);
-sr.reveal(`.home_handle`, {delay: 700});
-sr.reveal(`.home_social, .home_scroll`, {delay: 900, origin: 'bottom'});
+sr.reveal(`.intro-info`);
+sr.reveal(`.work`, {delay: 500, origin: 'bottom'});
+sr.reveal(`.testimonial`, {delay: 500});
+sr.reveal(`.about`, {delay: 800});
+sr.reveal(`.footer_container`, {delay: 900})
